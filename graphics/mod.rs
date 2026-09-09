34556f780b748386180288d30c8b01fccb5a7994
kauearaujodev <dextercurioso169ka@gmail.com>
2026-09-08 23:47:03 -0300

    Add new modules for GPU context, shader, and VRAM

--- graphics/mod.rs
+++ graphics/mod.rs
@@ -1,5 +1,13 @@
 pub mod framebuffer;
+pub mod gpu_context;
 pub mod renderer;
+pub mod shader;
+pub mod vram;
+pub mod vulkan;
 
 pub use framebuffer::FrameBuffer;
+pub use gpu_context::GpuContext;
 pub use renderer::Renderer;
+pub use shader::Shader;
+pub use vram::Vram;
+pub use vulkan::VulkanBackend;
